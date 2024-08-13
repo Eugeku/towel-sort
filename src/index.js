43bacbbd980e33d545fcc39d49080402961ce318
Eugeku <eugenekuzora@gmail.com>
2024-08-13 20:01:45 +0300

    feat: implement logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix == undefined) return [];
+  
+  let counter = 0;
+  return matrix.map(element => {
+    let array = [];
+    if (counter % 2 == 0) {
+      array = array.concat(element);
+    } else {
+      array = array.concat(element.reverse());
+    }
+    counter++;
+    return array;
+  }).flat();
 }
